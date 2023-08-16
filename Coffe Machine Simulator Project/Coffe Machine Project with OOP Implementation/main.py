cad1d129ff7bb8742116b138bc28320d2678af4a
yigitisik <86640635+yigitisik@users.noreply.github.com>
2023-08-16 00:17:15 -0400

    Add files via upload

---
+++ Coffe Machine Simulator Project/Coffe Machine Project with OOP Implementation/main.py
@@ -0,0 +1,21 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+coffeemaker = CoffeeMaker()
+moneymachine = MoneyMachine()
+menu = Menu()
+
+is_on = True
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}):")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffeemaker.report()
+        moneymachine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffeemaker.is_resource_sufficient(drink) and moneymachine.make_payment(drink.cost):
+            coffeemaker.make_coffee(drink)
